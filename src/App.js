cf5fa67d940eda306cb69d61504de9dd0b6924d4
FrankiePower <franklin.power21@gmail.com>
2024-06-09 11:48:38 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import web3 from "./web3";
+import lottery from "./lottery";
+
+//Good place to start is to try to get the address of the manager.
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { manager: "" };
+  }
+
+  // automatically called once component is rendered.
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    this.setState({ manager });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>This contract is managed by {this.state.manager}</p>
+      </div>
+    );
+  }
+}
+export default App;
